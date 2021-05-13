e2251dd7315ce40fd67fc0f9011aa1f37e193eec
Abdullah DÜNDAR <64316056+abdullahD00@users.noreply.github.com>
2021-05-14 01:19:51 +0300

    2_boyutlu_dizi

---
+++ 2Boyutlu_C.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include<stdlib.h>
+#define SIZE 5
+
+/*
+int main() {
+
+int dizi[2][2];
+int ortalama,toplam;
+toplam=0;
+dizi[0][0]=5;
+dizi[1][0]=10;
+dizi[0][1]=15;
+dizi[1][1]=10;
+
+toplam=dizi[0][0]+dizi[1][0]+dizi[0][1]+dizi[1][1];
+ortalama=toplam/4;
+printf("ortalama sonucu:%d",ortalama);
+
+
+
+
+	return 0;
+}
+
+burasý hatalý tekrar bak!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+int main() {
+int dizi[2][2];
+int enb,enk;
+dizi[0][0]=5;
+dizi[1][0]=10;
+dizi[0][1]=15;
+dizi[1][1]=20;
+if(dizi[0][0]>dizi[1][0] && dizi[0][1] && dizi[1][1])
+{
+	printf("en buyuk %d'dir.	",dizi[0][0]);
+}
+if(dizi[1][0]>dizi[0][0] && dizi[0][1] && dizi[1][1])
+{
+	printf("en buyuk %d'dir.",dizi[1][0]);
+}
+if(dizi[0][1]>dizi[0][0] && dizi[1][1] && dizi[1][0])
+{
+	printf("en buyuk %d'dir.",dizi[0][1]);
+}
+if(dizi[1][1]>dizi[0][1] && dizi[1][0] && dizi[0][0])
+ {
+ 	printf("en buyuk %d'dir.",dizi[1][1]);
+ }
+	return 0;
+}
+
+
+
+
+
+int main() {
+
+int notlar[SIZE]={0};
+int sum=0;
+int i;
+float ortalama=0;
+
+
+for(i=0;i<SIZE;i++)
+{
+	printf("\n%d. ogrenci notunu giriniz: ",i+1);
+	scanf("%d",&notlar[i]);
+}
+
+for(i=0;i<SIZE;i++)
+{
+	printf("%d. ogrencinin notu:%d\n",i+1,notlar[i]);
+	sum+=notlar[i];
+	
+}
+ortalama=(float)sum/SIZE;
+printf("notlar toplami	:	%d\n",sum);
+printf("notlar ortalamasi:	%f\n",ortalama);
+
+
+
+
+
+
+
+
+
+return 0;
+}
+*/
+
+int main() {
+
+
+
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
